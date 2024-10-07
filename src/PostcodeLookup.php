2942855309a3c44d31ca47f65d378f4ba9baf7b8
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-10-07 17:25:43 +0000

    Read indiviual values from database, without caching

--- src/PostcodeLookup.php
+++ src/PostcodeLookup.php
@@ -11,53 +11,18 @@ use PhpOffice\PhpSpreadsheet\Reader;
  * Perform postcode lookup.
  */
 class PostcodeLookup {
-  protected $data;
-
-  /**
-   * The cache backend.
-   *
-   * @var \Drupal\Core\Cache\CacheBackendInterface
-   */
-  protected $cacheBackend;
-
-  /**
-   * Constructs a new PostcodeLookup.
-   *
-   * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
-   *   The cache backend.
-   */
-  public function __construct(CacheBackendInterface $cache_backend) {
-    $this->cacheBackend = $cache_backend;
-  }
-
-  public function loadData () {
-    $last_imported_file = \Drupal::config('webform_postcode_austria.settings')->get('last_imported_file');
-    if (!$last_imported_file) {
-      webform_postcode_austria_import_data();
+  public function getPostcode(string $postcode) {
+    if (!$postcode) {
+      return null;
     }
 
     $connection = Database::getConnection();
     $query = $connection->select('webform_postcode_austria', 'c')
       ->fields('c')
+      ->condition('plz', $postcode)
       ->execute();
 
-    $this->data = $query->fetchAllAssoc('plz'); // Fetch all results as an associative array keyed by 'id'.
-  }
-
-  public function getPostcode(string $postcode) {
-    if (!$this->data) {
-      $this->loadData();
-    }
-
-    if (!$this->data) {
-      return null;
-    }
-
-    if (!array_key_exists($postcode, $this->data)) {
-      return null;
-    }
-
-    return $this->data[$postcode];
+    return $query->fetchAssoc() ?: null;
   }
 
 }
